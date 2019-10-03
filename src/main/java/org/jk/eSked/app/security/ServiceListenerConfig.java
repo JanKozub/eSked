2bcd1005406849e2b209ab007cd306762ba0d054
Jan <j.kozub136@gmail.com>
2019-10-03 10:22:14 +0200

    -preparation for spring boot security

---
+++ src/main/java/org/jk/eSked/app/security/ServiceListenerConfig.java
@@ -0,0 +1,26 @@
+package org.jk.eSked.app.security;
+
+import com.vaadin.flow.component.UI;
+import com.vaadin.flow.router.BeforeEnterEvent;
+import com.vaadin.flow.server.ServiceInitEvent;
+import com.vaadin.flow.server.VaadinServiceInitListener;
+import org.jk.eSked.ui.views.LoginView;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServiceListenerConfig implements VaadinServiceInitListener {
+    @Override
+    public void serviceInit(ServiceInitEvent event) {
+        event.getSource().addUIInitListener(uiEvent -> {
+            final UI ui = uiEvent.getUI();
+            ui.addBeforeEnterListener(this::beforeEnter);
+        });
+    }
+
+    private void beforeEnter(BeforeEnterEvent event) {
+        if (!LoginView.class.equals(event.getNavigationTarget())
+                && !SecurityUtils.isUserLoggedIn()) {
+            event.rerouteTo(LoginView.class);
+        }
+    }
+}
