a8cdc5fd92666d07eeb2ef9c6f4d41cc14584f1b
jankozub <j.kozub136@gmail.com>
2024-03-20 00:25:56 +0100

    -cleaned LoginView -cleaned MainLayout

--- src/main/java/org/jk/esked/app/frontend/views/LoginView.java
+++ src/main/java/org/jk/esked/app/frontend/views/LoginView.java
@@ -1,9 +1,8 @@
 package org.jk.esked.app.frontend.views;
 
-import com.vaadin.flow.component.Text;
-import com.vaadin.flow.component.dialog.Dialog;
+import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
-import com.vaadin.flow.component.icon.Icon;
+import com.vaadin.flow.component.html.Span;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.login.LoginOverlay;
@@ -21,32 +20,18 @@ import org.jk.esked.app.frontend.components.LoginExceptionDialog;
 @Route("login")
 @PageTitle("Login | eSked")
 @AnonymousAllowed
+@CssImport("styles/login.css")
 public class LoginView extends VerticalLayout implements BeforeEnterObserver {
     private final LoginOverlay login = new LoginOverlay(createI18n());
 
     public LoginView(UserService userService, EmailService emailService, EncryptionService encryptionService) {
-        login.setTitle(createTitle());
+        login.setTitle(new H1(VaadinIcon.CALENDAR.create(), new Span(" eSked")));
         login.setDescription("");
         login.setAction("login");
         login.setOpened(true);
-        login.addForgotPasswordListener(f -> {
-            Dialog dialog = new LoginExceptionDialog(userService, emailService, encryptionService);
-            dialog.open();
-        });
+        login.addForgotPasswordListener(f -> new LoginExceptionDialog(userService, emailService, encryptionService).open());
 
         add(login);
-    }
-
-    private H1 createTitle() {
-        H1 title = new H1();
-        title.getStyle().set("color", "var(--lumo-base-color)");
-        Icon icon = VaadinIcon.CALENDAR.create();
-        icon.setSize("30px");
-        icon.getStyle().set("top", "-4px");
-        title.add(icon);
-        title.add(new Text(" eSked"));
-
-        return title;
     }
 
     private LoginI18n createI18n() {
--- src/main/java/org/jk/esked/app/frontend/views/MainLayout.java
+++ src/main/java/org/jk/esked/app/frontend/views/MainLayout.java
@@ -7,14 +7,11 @@ import com.vaadin.flow.component.dependency.CssImport;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.icon.VaadinIcon;
-import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-import com.vaadin.flow.component.tabs.Tab;
-import com.vaadin.flow.router.RouterLink;
-import com.vaadin.flow.theme.lumo.LumoUtility;
 import org.jk.esked.app.backend.security.SecurityService;
+import org.jk.esked.app.frontend.components.DrawerTab;
 
-@CssImport("./styles/styles.css")
+@CssImport("styles/styles.css")
 public class MainLayout extends AppLayout {
     private final SecurityService securityService;
 
@@ -25,47 +22,28 @@ public class MainLayout extends AppLayout {
     }
 
     private void createHeader() {
-        HorizontalLayout header = new HorizontalLayout();
-        Image logo = new Image("icons/icon.png", "logo");
-        logo.setHeight("44px");
-        logo.getStyle().set("padding", "10px");
-        H1 title = new H1("eSked");
-        title.addClassNames(LumoUtility.FontSize.LARGE, LumoUtility.Margin.MEDIUM);
+        HorizontalLayout header = new HorizontalLayout(new Image("icons/icon.png", "logo"), new H1("eSked"));
+        header.addClassName("header");
 
         Button settings = new Button(VaadinIcon.COG_O.create(), e -> UI.getCurrent().navigate("settings"));
-        header.add(logo, title, settings);
+        HorizontalLayout buttons = new HorizontalLayout(settings);
+        buttons.addClassName("buttons");
 
         if (securityService.getAuthenticatedUser().getUsername().equals("admin")) //TODO implement roles
-            header.add(new Button(VaadinIcon.CALC_BOOK.create(), e -> UI.getCurrent().navigate("admin")));
+            buttons.add(new Button(VaadinIcon.CALC_BOOK.create(), e -> UI.getCurrent().navigate("admin")));
 
-        Button logout = new Button(VaadinIcon.POWER_OFF.create(), e -> securityService.logout());
-        header.add(logout);
+        buttons.add(new Button(VaadinIcon.POWER_OFF.create(), e -> securityService.logout()));
 
-        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
-        header.expand(title);
-        header.setWidthFull();
-        header.addClassNames(LumoUtility.Padding.Vertical.NONE, LumoUtility.Padding.Horizontal.MEDIUM);
-
-        addToNavbar(header);
+        addToNavbar(header, buttons);
     }
 
     private void createDrawer() {
-        Tab schedule = new Tab(VaadinIcon.CALENDAR_O.create(),
-                new RouterLink(getTranslation("tab.schedule"), ScheduleView.class));
-        schedule.getStyle().set("font-size", "var(--lumo-font-size-l)");
-        Tab newScheduleEntry = new Tab(VaadinIcon.FILE_ADD.create(),
-                new RouterLink(getTranslation("tab.schedule.new"), NewScheduleEntryView.class));
-        newScheduleEntry.getStyle().set("font-size", "var(--lumo-font-size-l)");
-        Tab events = new Tab(VaadinIcon.CALENDAR_CLOCK.create(),
-                new RouterLink(getTranslation("tab.events"), EventsView.class));
-        events.getStyle().set("font-size", "var(--lumo-font-size-l)");
-        Tab newEvent = new Tab(VaadinIcon.FOLDER_ADD.create(),
-                new RouterLink(getTranslation("tab.events.new"), NewEventView.class));
-        newEvent.getStyle().set("font-size", "var(--lumo-font-size-l)");
-        Tab messages = new Tab(VaadinIcon.ENVELOPE_OPEN_O.create(),
-                new RouterLink(getTranslation("tab.messages"), MessagesView.class));
-        messages.getStyle().set("font-size", "var(--lumo-font-size-l)");
-
-        addToDrawer(schedule, newScheduleEntry, events, newEvent, messages);
+        addToDrawer(
+                new DrawerTab(VaadinIcon.CALENDAR_O, getTranslation("tab.schedule"), ScheduleView.class),
+                new DrawerTab(VaadinIcon.FILE_ADD, getTranslation("tab.schedule.new"), NewScheduleEntryView.class),
+                new DrawerTab(VaadinIcon.CALENDAR_CLOCK, getTranslation("tab.events"), EventsView.class),
+                new DrawerTab(VaadinIcon.FOLDER_ADD, getTranslation("tab.events.new"), NewEventView.class),
+                new DrawerTab(VaadinIcon.ENVELOPE_OPEN_O, getTranslation("tab.messages"), MessagesView.class)
+        );
     }
 }
